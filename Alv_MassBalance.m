fb53d3a2d8f4a1410a4ca71a1239b7ee6cf9977d
2140cml <36232943+2140cml@users.noreply.github.com>
2023-03-02 10:17:06 -0800

    Add files via upload

---
+++ Alv_MassBalance.m
@@ -0,0 +1,29 @@
+function P = Alv_MassBalance(V2,V1,dt,PcO2,PAO21,pars)
+% Physiological parameters
+    yO2 = pars(1); 
+    PB = pars(2); 
+    PH2O = pars(3);
+    R = pars(4);  
+    T = pars(5); 
+    Beta_p = pars(6);
+    k = pars(7);
+     
+% Alveolar mass balance
+PIO2 = yO2*(PB-PH2O);
+fact1 = (V2-V1)./V1;
+fact2 = (V2-V1)/dt;
+coeff = k*R*T*dt*Beta_p./V1;
+% Inspiration
+num1 = fact1.*(PIO2-PAO21) - coeff.*(PAO21-PcO2) + PAO21;
+
+%Expiration
+num2 = -coeff.*(PAO21-PcO2) + PAO21;
+    
+    %Inspiration
+    if (fact2 >= 0)
+        P = num1;
+    else
+    %Expiration
+        P =  num2;
+    end
+end
